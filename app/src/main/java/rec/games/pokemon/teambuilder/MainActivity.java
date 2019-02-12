ef0d5c44b88ae48b1f20684a090154859161e1f4
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-02-12 14:36:24 -0800

    Initial commit.

---
+++ app/src/main/java/rec/games/pokemon/teambuilder/MainActivity.java
@@ -0,0 +1,13 @@
+package rec.games.pokemon.teambuilder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
