25f5beb70a6c56bc5effa0b6f8d221421e76625a
Miles Young <mwyoung2@gmail.com>
2019-02-21 17:02:48 -0800

    Added settings activity

--- app/src/main/java/rec/games/pokemon/teambuilder/MainActivity.java
+++ app/src/main/java/rec/games/pokemon/teambuilder/MainActivity.java
@@ -2,10 +2,13 @@ package rec.games.pokemon.teambuilder;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -48,4 +51,22 @@ public class MainActivity extends AppCompatActivity
 
 		mViewModel.loadPokemonListJSON(pokemonListURL);
 	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
