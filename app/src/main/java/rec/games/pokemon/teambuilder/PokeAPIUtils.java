4c514851459dd4e8b1695d1fa11c2a69520bed7e
hammockt <hammocktrevor@gmail.com>
2019-02-28 15:49:01 -0800

    Start implementation of the Cache. Just load in the lists

--- app/src/main/java/rec/games/pokemon/teambuilder/PokeAPIUtils.java
+++ app/src/main/java/rec/games/pokemon/teambuilder/PokeAPIUtils.java
@@ -10,9 +10,12 @@ import java.io.Serializable;
 public class PokeAPIUtils
 {
 	private final static String POKE_API_BASE_URL = "https://pokeapi.co/api/v2/";
-	private final static String POKE_API_POKEMON_ENDPOINT = "pokemon";
 	private final static String POKE_API_LIMIT_PARAM = "limit";
 	private final static String POKE_API_OFFSET_PARAM = "offset";
+
+	private final static String POKE_API_POKEMON_ENDPOINT = "pokemon";
+	private final static String POKE_API_TYPE_ENDPOINT = "type";
+	private final static String POKE_API_MOVE_ENDPOINT = "move";
 
 	static class NamedAPIResourceList implements Serializable
 	{
@@ -101,20 +104,30 @@ public class PokeAPIUtils
 		return buildNamedAPIResourceListURL(POKE_API_POKEMON_ENDPOINT, limit, offset);
 	}
 
-	static NamedAPIResourceList parsePokemonListJSON(String pokemonListJSON)
+	static String buildTypeListURL(int limit, int offset)
 	{
-		Gson gson = new Gson();
-		return gson.fromJson(pokemonListJSON, NamedAPIResourceList.class);
+		return buildNamedAPIResourceListURL(POKE_API_TYPE_ENDPOINT, limit, offset);
 	}
 
-	static int getPokeId(String url)
+	static String buildMoveListURL(int limit, int offset)
+	{
+		return buildNamedAPIResourceListURL(POKE_API_MOVE_ENDPOINT, limit, offset);
+	}
+
+	static NamedAPIResourceList parseNamedAPIResourceListJSON(String namedAPIResourceListJSON)
+	{
+		Gson gson = new Gson();
+		return gson.fromJson(namedAPIResourceListJSON, NamedAPIResourceList.class);
+	}
+
+	static int getId(String url)
 	{
 		if(url == null)
 			return 0;
 
-		String pokeId = Uri.parse(url).getLastPathSegment();
-		if(pokeId != null)
-			return Integer.parseInt(pokeId);
+		String id = Uri.parse(url).getLastPathSegment();
+		if(id != null)
+			return Integer.parseInt(id);
 		return 0;
 	}
 }
