581d759c8739eedd3d924b41cf4a177b7cffcf3a
hammockt <hammocktrevor@gmail.com>
2019-03-05 20:44:34 -0800

    Hopefully the final implementation of the rate limiter

--- app/src/main/java/rec/games/pokemon/teambuilder/RateLimitInterceptor.java
+++ app/src/main/java/rec/games/pokemon/teambuilder/RateLimitInterceptor.java
@@ -3,122 +3,44 @@ package rec.games.pokemon.teambuilder;
 import android.support.annotation.NonNull;
 
 import java.io.IOException;
-import java.util.PriorityQueue;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import okhttp3.Interceptor;
-import okhttp3.Request;
 import okhttp3.Response;
 
 class RateLimitInterceptor implements Interceptor
 {
-	private final int limit;
-	private final long timePeriod;
-	private PriorityQueue<Long> responseTimeStamps;
-	private long queueTail;
-
-	private Lock readLock;
-	private Lock writeLock;
+	private Semaphore limiter;
 
 	//limit: maximum number of requests to handle over a given time period
 	//timePeriod: the time period in nanoseconds
 	//so the maximum call rate = limit/timePeriod
-	//also a limit <= # of threads in OkHTTP thread pool is a bad idea (i.e <= 5 and <= 64 when multiple hosts are involved)
-	//this means that 120 requests/minute is safer than 2 requests/second
-	RateLimitInterceptor(int limit, long timePeriod)
+	RateLimitInterceptor(final int limit, final long timePeriod)
 	{
-		this.limit = limit;
-		this.timePeriod = timePeriod;
-		responseTimeStamps = new PriorityQueue<>(limit);
-		queueTail = Long.MIN_VALUE;
+		limiter = new Semaphore(limit);
+		Thread thread = new Thread()
+		{
+			@Override
+			public void run()
+			{
+				sleepAtLeast(timePeriod);
 
-		ReadWriteLock tailReadWriteLock = new ReentrantReadWriteLock();
-		readLock = tailReadWriteLock.readLock();
-		writeLock = tailReadWriteLock.writeLock();
+				limiter.drainPermits();
+				limiter.release(limit);
+			}
+		};
+
+		thread.start();
 	}
 
-	//this is a correction algorithm that will process up to maximum api call rate requests over a given timePeriod
-	//it does this by keeping track of the last "limit" amount of requests over a given time period
-	//if we are firing too fast, then throttle, else fire away
 	@NonNull
 	@Override
 	public Response intercept(@NonNull Chain chain) throws IOException
 	{
-		Request request = chain.request();
+		limiter.acquireUninterruptibly();
 
-		//time before request is sent
-		boolean slept = false;
-		readLock.lock();
-
-		//if the queue is full, we may have to throttle
-		if(responseTimeStamps.size() >= limit)
-		{
-			long elapsedTime = getElapsedTime();
-
-			//if we are at or below the maximum api call rate, then elapsedTime >= NANO_SECONDS_IN_MINUTE
-			if(elapsedTime < timePeriod)
-			{
-				//it would be bad if we held onto the lock while we are sleeping (it will cause writes to queue up)
-				//since we aren't doing anything after this and we are done reading responseTimeStamps/queueTail, release the lock
-				slept = true;
-				readLock.unlock();
-
-				long minNanosecondsToThrottle = timePeriod - elapsedTime;
-				sleepAtLeast(minNanosecondsToThrottle);
-			}
-		}
-
-		//if we didn't sleep, then we still have the lock
-		if(!slept)
-			readLock.unlock();
-
-		//sending request
-		Response response = chain.proceed(request);
-
-		//time immediately after the response was received
-		long timeStamp = System.nanoTime();
-
-		//we are gonna mess with responseTimeStamps and queueTail
-		writeLock.lock();
-
-		//optimization, if we remove any entries that exceed our time window (NANO_SECONDS_IN_MINUTE)
-		//then our queue.size() will be more likely to be < limit. And if queue.size() < limit, then we don't need to throttle
-		while(!responseTimeStamps.isEmpty() && getSimulatedElapsedTime(timeStamp) > timePeriod)
-			responseTimeStamps.poll();
-
-		//lets guarantee that we are only storing the last limit amount of requests
-		while(responseTimeStamps.size() >= limit)
-			responseTimeStamps.poll();
-
-		responseTimeStamps.offer(timeStamp);
-		if(timeStamp > queueTail)
-			queueTail = timeStamp;
-
-		writeLock.unlock();
-
-		return response;
-	}
-
-	//we at least have a shared lock when we call this
-	//we also make sure that the queue.size() >= 2 before calling this
-	private long getElapsedTime()
-	{
-		long head = responseTimeStamps.peek();
-		long tail = queueTail;
-
-		return tail - head;
-	}
-
-	//we at least have a shared lock when we call this
-	//we also make sure that the queue is not empty before calling this
-	private long getSimulatedElapsedTime(long tail)
-	{
-		long head = responseTimeStamps.peek();
-
-		return tail - head;
+		return chain.proceed(chain.request());
 	}
 
 	//java makes no guarantees about how long a thread will sleep
