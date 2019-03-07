0d78d74090b2f0dd5ef8f21c2f1eb8e9e3dc3f16
Miles Young <mwyoung2@gmail.com>
2019-03-07 08:50:46 -0800

    Added FAB button

--- app/src/main/java/rec/games/pokemon/teambuilder/TeamListFragment.java
+++ app/src/main/java/rec/games/pokemon/teambuilder/TeamListFragment.java
@@ -3,13 +3,19 @@ package rec.games.pokemon.teambuilder;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class TeamListFragment extends Fragment
 {
+	private static final String TAG = TeamListFragment.class.getSimpleName();
+
+	FloatingActionButton mTeamFAB;
+
 	public TeamListFragment()
 	{
 
@@ -25,6 +31,18 @@ public class TeamListFragment extends Fragment
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
 	{
-		return inflater.inflate(R.layout.team_list, container, false);
+		View view = inflater.inflate(R.layout.team_list, container, false);
+
+		mTeamFAB = view.findViewById(R.id.team_FAB);
+		mTeamFAB.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Log.d(TAG, "FAB Clicked");
+			}
+		});
+
+		return view;
 	}
 }
