492ea4442603b581b21789bf5ae4bce250d3d957
Miles Young <mwyoung2@gmail.com>
2019-03-07 16:41:37 -0800

    Fix layout in TeamFragment.java

--- app/src/main/java/rec/games/pokemon/teambuilder/TeamFragment.java
+++ app/src/main/java/rec/games/pokemon/teambuilder/TeamFragment.java
@@ -18,7 +18,7 @@ public class TeamFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_team, container, false);
+        View view = inflater.inflate(R.layout.team_list, container, false);
 
         adapter = new TeamAdapter(this);
         teamRV = view.findViewById(R.id.rv_team_members);
